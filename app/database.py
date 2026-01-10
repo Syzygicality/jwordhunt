3f164823f2909e16a675190dc5d6158b2242b0e5
Edison Wang <ew040607@gmail.com>
2026-01-10 14:15:42 -0500

    refactor mongodb connection

--- app/database.py
+++ app/database.py
@@ -1,8 +1,30 @@
 import certifi
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+from os import environ
 
-MONGO_URI = "mongodb+srv://ew040607_db_user:oGW1VLLo3Nj5eWn3@deltahacks.n3ffzak.mongodb.net/?appName=DeltaHacks"
+load_dotenv()
+
+def getenv(config: str) -> str:
+    """
+    Validates whether or not database env values exist in .env. If not, throws a value error.
+
+    :param config: Variable in .env
+    :type config: str
+    :return: Value of variable in .env
+    :rtype: str
+    """
+    value = environ.get(config)
+    if not value:
+        raise ValueError(f"{config} is missing from .env.")
+    return value
+
+DB_USER = getenv("DB_USER")
+DB_PASSWORD = getenv("DB_PASSWORD")
+DB_LOCATION = getenv("DB_LOCATION")
+
+MONGO_URL = f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@{DB_LOCATION}/?appName=DeltaHacks"
 DB_NAME = "jwordhunt"
 
 client: AsyncIOMotorClient | None = None
@@ -11,7 +33,7 @@ db = None
 def connect_to_mongo():
     global client, db
     client = AsyncIOMotorClient(
-        MONGO_URI,
+        MONGO_URL,
         server_api=ServerApi("1"),
         tls=True,
         tlsCAFile=certifi.where(),
