83cc1d60d16b44585bcd0b528e44662b61f21ab2
Edison Wang <ew040607@gmail.com>
2026-01-10 20:46:05 -0500

    second attempt

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ from app.routes import auth, chats, enums, users
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from beanie import init_beanie
+import os
+import uvicorn
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -19,3 +21,7 @@ app.include_router(auth.router)
 app.include_router(enums.router)
 app.include_router(chats.router)
 app.include_router(users.router)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
