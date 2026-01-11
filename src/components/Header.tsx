0beddbeb450e2f51a659ea22598af16b5d22fb00
Raymond Zhang <dracorz401@gmail.com>
2026-01-10 22:27:30 -0500

    fix cta button colour

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -129,7 +129,7 @@ export default function Header() {
                         <div className='mt-10 flex items-center justify-center gap-x-6'>
                             <Link
                                 to='/register'
-                                className='rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
+                                className='rounded-md bg-stone-800 px-4 py-3 text-sm font-semibold text-white shadow-xs hover:bg-stone-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
                             >
                                 Get started
                             </Link>
